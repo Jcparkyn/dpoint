3d0d79643aff3a6849967340d355a2849091d3c1
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2023-05-20 11:12:28 +1000

    Format python code

--- python/app.py
+++ python/app.py
@@ -22,11 +22,12 @@ from marker_tracker import run_tracker
 IMAGE_SHAPE = (600, 800)  # (height, width)
 CANVAS_SIZE = (1080, 1080)  # (width, height)
 NUM_LINE_POINTS = 400
-TRAIL_POINTS = 100
+TRAIL_POINTS = 1000
 
 COLORMAP_CHOICES = ["viridis", "reds", "blues"]
 LINE_COLOR_CHOICES = ["black", "red", "blue"]
 stylus_len = 0.143
+
 
 def append_line_point(line: np.ndarray, new_point: np.array):
     """Append new points to a line."""
@@ -34,46 +35,66 @@ def append_line_point(line: np.ndarray, new_point: np.array):
     line[-1, :] = new_point
     return line
 
+
 def get_line_color(line: np.ndarray):
     base_col = np.array([[1.0, 0.0, 0.0]], dtype=np.float32)
-    pos_z = line[:,[2]]
-    return np.hstack([
-        np.tile(base_col, (line.shape[0], 1)),
-        1 - np.clip(pos_z*200, 0, 1),
-    ])
+    pos_z = line[:, [2]]
+    return np.hstack(
+        [
+            np.tile(base_col, (line.shape[0], 1)),
+            1 - np.clip(pos_z * 200, 0, 1),
+        ]
+    )
+
 
 class CanvasWrapper:
     def __init__(self, eng):
         self._eng = eng
-        self.canvas = SceneCanvas(size=CANVAS_SIZE, keys='interactive')
+        self.canvas = SceneCanvas(size=CANVAS_SIZE, keys="interactive")
         self.canvas.measure_fps()
         self.grid = self.canvas.central_widget.add_grid()
 
-        self.view_top = self.grid.add_view(0, 0, bgcolor='grey')
-        self.view_top.camera = scene.TurntableCamera(up='z', fov=0)
-        self.cube = visuals.Box(0.014, stylus_len, 0.014, edge_color="black",
-                         parent=self.view_top.scene)
+        self.view_top = self.grid.add_view(0, 0, bgcolor="grey")
+        self.view_top.camera = scene.TurntableCamera(up="z", fov=0)
+        self.cube = visuals.Box(
+            0.014, stylus_len, 0.014, edge_color="black", parent=self.view_top.scene
+        )
         pen_tip = visuals.XYZAxis(parent=self.cube)
-        pen_tip.transform = transforms.MatrixTransform(vispy.util.transforms.scale([0.02, 0.02, 0.02]) @ vispy.util.transforms.translate([0, 0, stylus_len*0.5]))
-        self.cube.transform = transforms.MatrixTransform(vispy.util.transforms.translate([0, 0, 0]))
+        pen_tip.transform = transforms.MatrixTransform(
+            vispy.util.transforms.scale([0.02, 0.02, 0.02])
+            @ vispy.util.transforms.translate([0, 0, stylus_len * 0.5])
+        )
+        self.cube.transform = transforms.MatrixTransform(
+            vispy.util.transforms.translate([0, 0, 0])
+        )
 
         trail_data = np.zeros((TRAIL_POINTS, 3), dtype=np.float32)
-        self.trail_line = visuals.Line(pos=trail_data, color="red", width=2, parent=self.view_top.scene)
+        self.trail_line = visuals.Line(
+            pos=trail_data, color="red", width=2, parent=self.view_top.scene
+        )
 
         axis = scene.visuals.XYZAxis(parent=self.view_top.scene)
 
     def update_data(self, new_data_dict):
         orientation = new_data_dict["orientation"]
-        orientation_quat = quaternion.Quaternion(orientation[0], orientation[1], orientation[3], orientation[2])
+        orientation_quat = quaternion.Quaternion(
+            orientation[0], orientation[1], orientation[3], orientation[2]
+        )
         or_fix = vispy.util.transforms.rotate(90, [1, 0, 0])
         pos = new_data_dict["position"]
         pos_converted = np.array([-pos[0], pos[1], -pos[2]])
-        self.cube.transform.matrix = vispy.util.transforms.translate([0, 0, stylus_len*0.5]) @ orientation_quat.get_matrix() @ or_fix @ vispy.util.transforms.translate(pos_converted)
+        self.cube.transform.matrix = (
+            vispy.util.transforms.translate([0, 0, stylus_len * 0.5])
+            @ orientation_quat.get_matrix()
+            @ or_fix
+            @ vispy.util.transforms.translate(pos_converted)
+        )
         self.trail_line.set_data(
             append_line_point(self.trail_line.pos, pos_converted),
             color=get_line_color(self.trail_line.pos),
         )
         # self.canvas.update()
+
 
 class MainWindow(QtWidgets.QMainWindow):
     closing = QtCore.pyqtSignal()
@@ -95,11 +116,12 @@ class MainWindow(QtWidgets.QMainWindow):
         self.closing.emit()
         return super().closeEvent(event)
 
+
 def readVals(device: serial.Serial):
-    while device.in_waiting > 6*7:
-      device.readline()
+    while device.in_waiting > 6 * 7:
+        device.readline()
     line = device.readline().decode().rstrip()
-    vals = np.array(line.split(','), dtype=np.float32)
+    vals = np.array(line.split(","), dtype=np.float32)
     if vals.size != 6:
         vals = np.zeros(6, dtype=np.float32)
     accel = vals[0:3] * 9.8
@@ -108,11 +130,19 @@ def readVals(device: serial.Serial):
     # gyro = [gyro[2], gyro[1], -gyro[3]];
     return accel, gyro
 
+
 class ImuDataSource(QtCore.QObject):
     new_data = QtCore.pyqtSignal(dict)
     finished = QtCore.pyqtSignal()
 
-    def __init__(self, eng: matlab.engine.MatlabEngine, imuf, tracker_queue: mp.Queue, num_iterations=50000, parent=None):
+    def __init__(
+        self,
+        eng: matlab.engine.MatlabEngine,
+        imuf,
+        tracker_queue: mp.Queue,
+        num_iterations=50000,
+        parent=None,
+    ):
         super().__init__(parent)
         self._should_end = False
         self._num_iters = num_iterations
@@ -122,21 +152,23 @@ class ImuDataSource(QtCore.QObject):
 
     def run_data_creation(self):
         print("Run data creation is starting")
-        device = serial.Serial('COM3', 250000)
+        device = serial.Serial("COM3", 250000)
         lastTime = time.time()
         task = None
-        imu_queue = deque() # Add artificial delay to match camera feed
+        imu_queue = deque()  # Add artificial delay to match camera feed
         for _ in range(self._num_iters):
             if self._should_end:
                 print("Data source saw that it was told to stop")
                 break
-            
+
             try:
                 # print(f"Queue size: {self._tracker_queue.qsize()}")
                 while self._tracker_queue.qsize() > 3:
                     self._tracker_queue.get()
                 camera_position, camera_orientation = self._tracker_queue.get_nowait()
-                self._eng.update_tracker(self._imuf, camera_position, camera_orientation, nargout=0)
+                self._eng.update_tracker(
+                    self._imuf, camera_position, camera_orientation, nargout=0
+                )
             except queue.Empty:
                 pass
 
@@ -144,16 +176,20 @@ class ImuDataSource(QtCore.QObject):
             imu_queue.append((accel, gyro))
             if task is not None:
                 (position, orientation) = task.result()
-                self.new_data.emit({
-                    "position": list(position[0]),
-                    "orientation": list(orientation[0]),
-                })
+                self.new_data.emit(
+                    {
+                        "position": list(position[0]),
+                        "orientation": list(orientation[0]),
+                    }
+                )
 
             if len(imu_queue) > 3:
                 accel, gyro = imu_queue.popleft()
                 currentTime = time.time()
                 dt = currentTime - lastTime
-                task = self._eng.step(self._imuf, accel, gyro, dt, nargout=2, background=True)
+                task = self._eng.step(
+                    self._imuf, accel, gyro, dt, nargout=2, background=True
+                )
                 # task.done
                 lastTime = currentTime
         print("Data source finishing")
@@ -165,7 +201,10 @@ class ImuDataSource(QtCore.QObject):
 
 
 def run_tracker_with_queue(queue: mp.Queue):
-    run_tracker(lambda orientation, position: queue.put((orientation, position), block=False))
+    run_tracker(
+        lambda orientation, position: queue.put((orientation, position), block=False)
+    )
+
 
 if __name__ == "__main__":
     app = use_app("pyqt6")
@@ -190,9 +229,13 @@ if __name__ == "__main__":
     data_thread.started.connect(data_source.run_data_creation)
     # camera_thread.started.connect(camera_data_source.run_data_creation)
     # if the data source finishes before the window is closed, kill the thread
-    data_source.finished.connect(data_thread.quit, QtCore.Qt.ConnectionType.DirectConnection)
+    data_source.finished.connect(
+        data_thread.quit, QtCore.Qt.ConnectionType.DirectConnection
+    )
     # if the window is closed, tell the data source to stop
-    win.closing.connect(data_source.stop_data, QtCore.Qt.ConnectionType.DirectConnection)
+    win.closing.connect(
+        data_source.stop_data, QtCore.Qt.ConnectionType.DirectConnection
+    )
     # when the thread has ended, delete the data source from memory
     data_thread.finished.connect(data_source.deleteLater)
 
--- python/marker_tracker.py
+++ python/marker_tracker.py
@@ -5,6 +5,7 @@ from typing import Tuple, Callable, Optional
 import time
 import sys
 
+
 def readCameraParameters(filename: str) -> Tuple[np.ndarray, np.ndarray]:
     fs = cv2.FileStorage(filename, cv2.FILE_STORAGE_READ)
     if not fs.isOpened():
@@ -13,52 +14,64 @@ def readCameraParameters(filename: str) -> Tuple[np.ndarray, np.ndarray]:
     distCoeffs = fs.getNode("distortion_coefficients").mat()
     return (cameraMatrix, distCoeffs)
 
+
 def getWebcam():
-  webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-  webcam.set(cv2.CAP_PROP_FPS, 60)
-  webcam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-  webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-  if not webcam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG')):
-      raise Exception("Couldn't set FourCC")
-  webcam.set(cv2.CAP_PROP_AUTOFOCUS, 0)
-  webcam.set(cv2.CAP_PROP_FOCUS, 30)
-  webcam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
-  webcam.set(cv2.CAP_PROP_EXPOSURE, -7)
-  webcam.set(cv2.CAP_PROP_BRIGHTNESS, 127)
-  webcam.set(cv2.CAP_PROP_CONTRAST, 140)
-  webcam.set(cv2.CAP_PROP_GAIN, 200)
-  return webcam
+    webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    webcam.set(cv2.CAP_PROP_FPS, 60)
+    webcam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    if not webcam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG")):
+        raise Exception("Couldn't set FourCC")
+    webcam.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+    webcam.set(cv2.CAP_PROP_FOCUS, 30)
+    webcam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+    webcam.set(cv2.CAP_PROP_EXPOSURE, -7)
+    webcam.set(cv2.CAP_PROP_BRIGHTNESS, 127)
+    webcam.set(cv2.CAP_PROP_CONTRAST, 140)
+    webcam.set(cv2.CAP_PROP_GAIN, 200)
+    webcam.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+    return webcam
+
 
 def rotateY(angle: float, point: np.ndarray) -> np.ndarray:
     c, s = np.cos(angle), np.sin(angle)
     rotation_matrix = np.array([[c, 0, s], [0, 1, 0], [-s, 0, c]], dtype=np.float32)
     return np.dot(rotation_matrix, point)
 
+
 markerLength = 0.015
-markerCorners = np.array([
-    [-markerLength/2, markerLength/2, 0],
-    [markerLength/2, markerLength/2, 0],
-    [markerLength/2, -markerLength/2, 0],
-    [-markerLength/2, -markerLength/2, 0],
-  ], dtype=np.float32)
+markerCorners = np.array(
+    [
+        [-markerLength / 2, markerLength / 2, 0],
+        [markerLength / 2, markerLength / 2, 0],
+        [markerLength / 2, -markerLength / 2, 0],
+        [-markerLength / 2, -markerLength / 2, 0],
+    ],
+    dtype=np.float32,
+)
+
 
 def getCornersPS(origin: np.ndarray, angleY: float) -> np.ndarray:
     cornersWS = markerCorners + origin
     rotated_corners = np.apply_along_axis(lambda x: rotateY(angleY, x), 1, cornersWS)
     return rotated_corners
 
+
 def deg2rad(deg: float) -> float:
     return deg * np.pi / 180
 
+
 def inverseRT(rvec, tvec):
-   R, _ = cv2.Rodrigues(rvec)
-   Rt = np.transpose(R)
-   return (cv2.Rodrigues(Rt)[0], -Rt@tvec)
+    R, _ = cv2.Rodrigues(rvec)
+    Rt = np.transpose(R)
+    return (cv2.Rodrigues(Rt)[0], -Rt @ tvec)
+
 
 def relativeTransform(rvec1, tvec1, rvec2, tvec2) -> Tuple[np.ndarray, np.ndarray]:
-   baseRvec, baseTvec = inverseRT(rvec2, tvec2)
-   rvec, tvec, *_ = cv2.composeRT(rvec1, tvec1, baseRvec, baseTvec)
-   return (rvec, tvec)
+    baseRvec, baseTvec = inverseRT(rvec2, tvec2)
+    rvec, tvec, *_ = cv2.composeRT(rvec1, tvec1, baseRvec, baseTvec)
+    return (rvec, tvec)
+
 
 markersOnPen = {
     0: getCornersPS(np.array([0, 0.147, 0.01], dtype=np.float32), deg2rad(45)),
@@ -67,17 +80,21 @@ markersOnPen = {
     3: getCornersPS(np.array([0, 0.147, 0.01], dtype=np.float32), deg2rad(315)),
     4: getCornersPS(np.array([0, 0.1175, 0.011], dtype=np.float32), deg2rad(90)),
     5: getCornersPS(np.array([0, 0.1175, 0.011], dtype=np.float32), deg2rad(180)),
-    6: getCornersPS(np.array([0, 0.1175, 0.011], dtype=np.float32), deg2rad(270)),
+    6: getCornersPS(np.array([0, 0.1170, 0.011], dtype=np.float32), deg2rad(270)),
     7: getCornersPS(np.array([0, 0.1175, 0.011], dtype=np.float32), deg2rad(5)),
 }
 
 baseMarkerId = 40
 baseMarkerLength = 0.1
-baseMarkerCornersWS = np.array([
-    [-baseMarkerLength/2, baseMarkerLength/2, 0],
-    [baseMarkerLength/2, baseMarkerLength/2, 0],
-    [baseMarkerLength/2, -baseMarkerLength/2, 0],
-    [-baseMarkerLength/2, -baseMarkerLength/2, 0]], dtype=np.float32)
+baseMarkerCornersWS = np.array(
+    [
+        [-baseMarkerLength / 2, baseMarkerLength / 2, 0],
+        [baseMarkerLength / 2, baseMarkerLength / 2, 0],
+        [baseMarkerLength / 2, -baseMarkerLength / 2, 0],
+        [-baseMarkerLength / 2, -baseMarkerLength / 2, 0],
+    ],
+    dtype=np.float32,
+)
 
 arucoDic = aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
 arucoParams = aruco.DetectorParameters()
@@ -97,94 +114,118 @@ arucoParams.maxMarkerPerimeterRate = 1
 arucoParams.minSideLengthCanonicalImg = 16
 detector = aruco.ArucoDetector(arucoDic, arucoParams)
 
+
 def array_to_str(arr):
-  return ",".join(map(lambda x: f"{x*100:+2.1f}", list(arr.flat)))
+    return ",".join(map(lambda x: f"{x*100:+2.1f}", list(arr.flat)))
+
 
 def run_tracker(on_estimate: Optional[Callable[[np.ndarray, np.ndarray], None]]):
-  cv2.namedWindow("Tracker", cv2.WINDOW_KEEPRATIO)
-  cv2.moveWindow("Tracker", -1080, -200)
-  cameraMatrix, distCoeffs = readCameraParameters("camera_params_c920.yml")
-  print("Opening webcam..")
-  webcam = getWebcam()
+    cv2.namedWindow("Tracker", cv2.WINDOW_KEEPRATIO)
+    cv2.moveWindow("Tracker", -1080, -200)
+    cameraMatrix, distCoeffs = readCameraParameters("camera_params_c920.yml")
+    print("Opening webcam..")
+    webcam = getWebcam()
 
-  rvec, tvec = (None, None)
-  initialized = False
-  calibrated = False
-  baseRvec = np.zeros([3, 1])
-  baseTvec = np.zeros([3, 1])
-  avg_fps = 30
+    rvec, tvec = (None, None)
+    initialized = False
+    calibrated = False
+    baseRvec = np.zeros([3, 1])
+    baseTvec = np.zeros([3, 1])
+    avg_fps = 30
 
-  while(True):
-    frameStartTime = time.perf_counter()
-    keypress = cv2.waitKey(1) & 0xFF
-    if keypress == ord('q'):
-      break
-    elif keypress == ord('u'):
-      webcam.set(cv2.CAP_PROP_FOCUS, webcam.get(cv2.CAP_PROP_FOCUS) + 5)
-    elif keypress == ord('d'):
-      webcam.set(cv2.CAP_PROP_FOCUS, webcam.get(cv2.CAP_PROP_FOCUS) - 5)
+    while True:
+        frameStartTime = time.perf_counter()
+        keypress = cv2.waitKey(1) & 0xFF
+        if keypress == ord("q"):
+            break
+        elif keypress == ord("u"):
+            webcam.set(cv2.CAP_PROP_FOCUS, webcam.get(cv2.CAP_PROP_FOCUS) + 5)
+        elif keypress == ord("d"):
+            webcam.set(cv2.CAP_PROP_FOCUS, webcam.get(cv2.CAP_PROP_FOCUS) - 5)
 
-    ret, frame = webcam.read()
-    processingStartTime = time.perf_counter()
-    cv2.flip(frame, -1, frame)
+        ret, frame = webcam.read()
+        processingStartTime = time.perf_counter()
+        cv2.flip(frame, -1, frame)
 
-    ids: np.ndarray
-    allCornersIS: np.ndarray
-    allCornersIS, ids, rejected = detector.detectMarkers(frame)
-    aruco.drawDetectedMarkers(frame, allCornersIS, ids)
+        ids: np.ndarray
+        allCornersIS: np.ndarray
+        allCornersIS, ids, rejected = detector.detectMarkers(frame)
+        aruco.drawDetectedMarkers(frame, allCornersIS, ids)
 
-    validMarkers = []
-    if ids is not None:
-      for i in range(ids.shape[0]):
-        id, cornersIS = (ids[i,0], allCornersIS[i][0,:,:])
-        if id in markersOnPen:
-          cornersPS = markersOnPen[id]
-          validMarkers.append((id, cornersPS, cornersIS))
-      
-      if baseMarkerId in ids and (not calibrated or keypress == ord('c')):
-        calibrated = True
-        print("Calibrating...")
-        baseMarkerIndex = list(ids).index(baseMarkerId)
-        baseMarkerCornersIS = allCornersIS[baseMarkerIndex]
-        success, baseRvec, baseTvec = cv2.solvePnP(
-          baseMarkerCornersWS,
-          baseMarkerCornersIS,
-          cameraMatrix,
-          distCoeffs
+        validMarkers = []
+        if ids is not None:
+            for i in range(ids.shape[0]):
+                id, cornersIS = (ids[i, 0], allCornersIS[i][0, :, :])
+                if id in markersOnPen:
+                    cornersPS = markersOnPen[id]
+                    validMarkers.append((id, cornersPS, cornersIS))
+
+            if baseMarkerId in ids and (not calibrated or keypress == ord("c")):
+                calibrated = True
+                print("Calibrating...")
+                baseMarkerIndex = list(ids).index(baseMarkerId)
+                baseMarkerCornersIS = allCornersIS[baseMarkerIndex]
+                success, baseRvec, baseTvec = cv2.solvePnP(
+                    baseMarkerCornersWS, baseMarkerCornersIS, cameraMatrix, distCoeffs
+                )
+
+        if len(validMarkers) >= 1:
+            screenCorners = np.concatenate(
+                [cornersIS for _, _, cornersIS in validMarkers]
+            )
+            penCorners = np.concatenate([cornersPS for _, cornersPS, _ in validMarkers])
+            success, rvec, tvec = cv2.solvePnP(
+                penCorners,
+                screenCorners,
+                cameraMatrix=cameraMatrix,
+                distCoeffs=distCoeffs,
+                rvec=rvec,
+                tvec=tvec,
+                useExtrinsicGuess=initialized,
+                flags=cv2.SOLVEPNP_ITERATIVE,
+            )
+
+            rvecRelative, tvecRelative = relativeTransform(
+                rvec, tvec, baseRvec, baseTvec
+            )
+            Rrelative = cv2.Rodrigues(rvecRelative)[0]  # TODO: use Rodrigues directly
+            cv2.drawFrameAxes(frame, cameraMatrix, distCoeffs, rvec, tvec, markerLength)
+            cv2.putText(
+                frame,
+                f"Tip: [{array_to_str(tvecRelative)}]",
+                (10, 120),
+                cv2.FONT_HERSHEY_DUPLEX,
+                1,
+                (0, 255, 0),
+            )
+            if on_estimate is not None:
+                on_estimate(Rrelative, tvecRelative)
+            initialized = success
+        else:
+            initialized = False
+
+        frameEndTime = time.perf_counter()
+        fps = 1 / (frameEndTime - frameStartTime)
+        avg_fps = 0.9 * avg_fps + 0.1 * fps
+        cv2.putText(
+            frame,
+            f"FPS: {avg_fps:.1f}",
+            (10, 30),
+            cv2.FONT_HERSHEY_DUPLEX,
+            1,
+            (0, 255, 0),
+        )
+        cv2.putText(
+            frame,
+            f"Processing: {(frameEndTime - processingStartTime)*1000:.1f}ms",
+            (10, 60),
+            cv2.FONT_HERSHEY_DUPLEX,
+            1,
+            (0, 255, 0),
         )
 
-    if len(validMarkers) >= 1:
-      screenCorners = np.concatenate([cornersIS for _, _, cornersIS in validMarkers])
-      penCorners = np.concatenate([cornersPS for _, cornersPS, _ in validMarkers])
-      success, rvec, tvec = cv2.solvePnP(
-        penCorners,
-        screenCorners,
-        cameraMatrix=cameraMatrix,
-        distCoeffs=distCoeffs,
-        rvec=rvec,
-        tvec=tvec,
-        useExtrinsicGuess=initialized,
-        flags=cv2.SOLVEPNP_ITERATIVE
-      )
+        cv2.imshow("Tracker", frame)
 
-      rvecRelative, tvecRelative = relativeTransform(rvec, tvec, baseRvec, baseTvec)
-      Rrelative = cv2.Rodrigues(rvecRelative)[0] # TODO: use Rodrigues directly
-      cv2.drawFrameAxes(frame, cameraMatrix, distCoeffs, rvec, tvec, markerLength)
-      cv2.putText(frame, f"Tip: [{array_to_str(tvecRelative)}]", (10, 120), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0))
-      if on_estimate is not None:
-        on_estimate(Rrelative, tvecRelative)
-      initialized = success
-    else:
-      initialized = False
 
-    frameEndTime = time.perf_counter()
-    fps = 1/(frameEndTime - frameStartTime)
-    avg_fps = 0.9*avg_fps + 0.1*fps
-    cv2.putText(frame, f"FPS: {avg_fps:.1f}", (10, 30), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0))
-    cv2.putText(frame, f"Processing: {(frameEndTime - processingStartTime)*1000:.1f}ms", (10, 60), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0))
-
-    cv2.imshow('Tracker', frame)
-
-    
-if __name__ == '__main__' and sys.flags.interactive == 0:
-  run_tracker(None)
+if __name__ == "__main__" and sys.flags.interactive == 0:
+    run_tracker(None)
