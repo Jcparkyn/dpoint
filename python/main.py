4b8d1e5b1b69686154e92f6fc9c8dafca219b27d
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2023-11-20 19:12:59 +1100

    Add docstrings for top-level scripts

--- python/main.py
+++ python/main.py
@@ -1,4 +1,6 @@
 from app.app import main
 
+"""Runs the main application."""
+
 if __name__ == "__main__":
     main()
--- python/run_marker_tracker.py
+++ python/run_marker_tracker.py
@@ -1,5 +1,8 @@
 from app.marker_tracker import run_tracker
 
+"""
+This is a utility script to run the marker tracker without any of the other parts (BLE, filtering, GUI, etc).
+"""
 
 if __name__ == "__main__":
     run_tracker(None)
